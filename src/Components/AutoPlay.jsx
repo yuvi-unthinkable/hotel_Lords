5e663378adc80716b20bed3c57e55f9565134d29
yuvraj <yuvraj.tripathi@unthinkable.co>
2025-08-12 18:50:31 +0530

    taking the images from the daatabase for the dining an room slider

--- src/Components/AutoPlay.jsx
+++ src/Components/AutoPlay.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import Navbar from "./Navbar";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
 
 function AutoPlay({ dinner }) {
+  const [res, setRes] = useState()
+
+  useEffect( () => {
+    const getHotelImage = (async () => {
+
+      await axios.get("http://localhost:8000/api/v1/users/get-hotels", {
+        withCredentials : true,
+      })
+      .then((temp) => {
+        setRes(temp.data);
+        console.log("Hi this it the response data from autoplay   ",res);
+
+      })
+    })
+    getHotelImage()
+
+
+      
+    
+  },[])
 
   const images = !dinner ? [
     "https://assets.simplotel.com/simplotel/image/upload/x_59,y_0,w_1231,h_600,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/image_2025-03-24-13-23-37_67e15cd916f00",
@@ -14,12 +35,9 @@ function AutoPlay({ dinner }) {
     "https://assets.simplotel.com/simplotel/image/upload/x_0,y_547,w_4160,h_2026,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/Ankleshwar_7851ed9a",
     "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3333/x_0,y_449,w_5000,h_2435,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-inn-udaipur/Lords_Udaipur,79,2022_937c657a",
     "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3333/x_0,y_449,w_5000,h_2435,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/Lords_Ambaji_2024,1_9e81b4ec",
-  ] : [
-    "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3123/x_419,y_0,w_4162,h_3123,r_0,c_crop/q_80,w_900,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/Restaurant_&_Bar_5591c234",
-    "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3333/x_279,y_0,w_4442,h_3333,r_0,c_crop/q_80,w_900,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/Lords_Ambaji_2024,22_8f9d25af",
-    "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3337/x_276,y_0,w_4448,h_3337,r_0,c_crop/q_80,w_900,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/BCR_2_09cde0ff",
-    "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3333/x_279,y_0,w_4442,h_3333,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/Lords_Ambaji_2024,22_8f9d25af",
-  ]
+  ] : res?.data?.find(hotel => hotel.hotelName === "hotel-display")?.photos?.map(photo=> photo.url) || []
+  
+
 
   const settings = {
     dots: false,
--- src/Components/RoomSLider.jsx
+++ src/Components/RoomSLider.jsx
@@ -3,41 +3,67 @@ import Card from "./Card";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
 
-export const srcArray = [
-  {
-    source:
-      "https://assets.simplotel.com/simplotel/image/upload/x_0,y_113,w_1280,h_960,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-inn-zirakpur/Exterior_92c349a2",
-    data: "Rang Lords Inn Bharuch",
-  },
-  {
-    source:
-      "https://assets.simplotel.com/simplotel/image/upload/w_4378,h_5000/x_0,y_392,w_4378,h_3285,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-eco-inn-dehradun/Facade_25c29c01",
-    data: "Angel Lords Eco Inn Amreli",
-  },
-  {
-    source:
-      "https://assets.simplotel.com/simplotel/image/upload/x_0,y_369,w_2640,h_1982,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/sk-lords-inn-ahmedabad/Exterior_cc2abc66",
-    data: "Lords Eco Inn Dahej",
-  },
-  {
-    source:
-      "https://assets.simplotel.com/simplotel/image/upload/x_0,y_33,w_916,h_688,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/the_perfect_resort_escape__celebrate_the_new_year_away_from_the_city_rush_image_3da219f1",
-    data: "Lords Eco Inn Jayanagar Bangalore",
-  },
-  {
-    source:
-      "https://assets.simplotel.com/simplotel/image/upload/x_0,y_280,w_959,h_720,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/WhatsApp_Image_2025-04-30_at_11.38.00_AM_a77da884",
-    data: "Lords Hrim Akshara Resort Basar",
-  },
-  {
-    source:
-      "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3337/x_276,y_0,w_4448,h_3337,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-eco-inn-morbi/Facade_(5)_bf8c1af4",
-    data: "Lords Inn Rajkot",
-  },
-];
+// export const srcArray = [
+//   {
+//     source:
+//       "https://assets.simplotel.com/simplotel/image/upload/x_0,y_113,w_1280,h_960,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-inn-zirakpur/Exterior_92c349a2",
+//     data: "Rang Lords Inn Bharuch",
+//   },
+//   {
+//     source:
+//       "https://assets.simplotel.com/simplotel/image/upload/w_4378,h_5000/x_0,y_392,w_4378,h_3285,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-eco-inn-dehradun/Facade_25c29c01",
+//     data: "Angel Lords Eco Inn Amreli",
+//   },
+//   {
+//     source:
+//       "https://assets.simplotel.com/simplotel/image/upload/x_0,y_369,w_2640,h_1982,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/sk-lords-inn-ahmedabad/Exterior_cc2abc66",
+//     data: "Lords Eco Inn Dahej",
+//   },
+//   {
+//     source:
+//       "https://assets.simplotel.com/simplotel/image/upload/x_0,y_33,w_916,h_688,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/the_perfect_resort_escape__celebrate_the_new_year_away_from_the_city_rush_image_3da219f1",
+//     data: "Lords Eco Inn Jayanagar Bangalore",
+//   },
+//   {
+//     source:
+//       "https://assets.simplotel.com/simplotel/image/upload/x_0,y_280,w_959,h_720,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-hotels-resorts/WhatsApp_Image_2025-04-30_at_11.38.00_AM_a77da884",
+//     data: "Lords Hrim Akshara Resort Basar",
+//   },
+//   {
+//     source:
+//       "https://assets.simplotel.com/simplotel/image/upload/w_5000,h_3337/x_276,y_0,w_4448,h_3337,r_0,c_crop/q_80,w_1600,dpr_1,f_auto,fl_progressive,c_limit/lords-eco-inn-morbi/Facade_(5)_bf8c1af4",
+//     data: "Lords Inn Rajkot",
+//   },
+// ];
 
 export default function RoomSLider() {
+  const [res, setRes] = useState();
+
+  useEffect( () => {
+    const fetchHotel = (async() => {
+    try {
+      axios
+        .get("http://localhost:8000/api/v1/users/get-hotels", {
+          withCredentials: true,
+        })
+        .then((temp)=> {
+
+          setRes(temp.data)
+          // console.log("🚀 ~ RoomSLider ~ res.data:", res)
+        })
+        
+        
+    } catch (error) {
+      console.log("🚀 ~ error in getting hotel details:", error);
+    }
+  })
+  fetchHotel()
+  }, []);
+
+  // console.log("here is the fetched response data ", res?.data);
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const settings = {
@@ -54,15 +80,19 @@ export default function RoomSLider() {
     },
   };
 
-  useEffect(() => {
-    console.log("Current Slide Index:", currentIndex);
-  }, [currentIndex]);
+  // useEffect(() => {
+  //   console.log("Current Slide Index:", currentIndex);
+  // }, [currentIndex]);
 
   return (
     <div className="room-slider">
       <Slider {...settings}>
-        {srcArray.map((card, index) => (
-          <Card key={index} source={card.source} data={card.data} />
+        {res?.data?.map((hotel, index) => (
+          <Card
+            key={index}
+            source={hotel.photos?.[0]?.url }
+            data={hotel?.hotelName}
+          />
         ))}
       </Slider>
     </div>
