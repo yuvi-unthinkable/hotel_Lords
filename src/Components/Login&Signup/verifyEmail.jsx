552186c7ca894f3578eb1c2f828f35be26b4e7ef
yuvraj <yuvraj.tripathi@unthinkable.co>
2025-08-27 17:07:28 +0530

    test update for email verification

--- src/Components/Login&Signup/verifyEmail.jsx
+++ src/Components/Login&Signup/verifyEmail.jsx
@@ -1,32 +1,31 @@
-// import React from 'react';
 
-// function verifyEmail() {
-//   const [message, setMessage] = useState('Verifying...');
-//   const searchParams = new URLSearchParams(window.location.search);
-//   const token = searchParams.get('token');
+function verifyEmail() {
+  const [message, setMessage] = useState('Verifying...');
+  const searchParams = new URLSearchParams(window.location.search);
+  const token = searchParams.get('token');
 
-//   useEffect(() => {
-//     const verify = async () => {
-//       try {
-//         await axios.get(`/api/v1/users/verify/:token`);
-//         setMessage('Email verified successfully!');
-//         console.log(message);
-//       } catch {
-//         setMessage('Verification failed. The link may have expired.');
-//         console.log(message);
-//       }
-//     };
-//     verify();
-//   }, [token]);
+  useEffect(() => {
+    const verify = async () => {
+      try {
+        await axios.get(`https://chai-and-backend.onrender.com/api/v1/users/verify/:token`);
+        setMessage('Email verified successfully!');
+        console.log(message);
+      } catch {
+        setMessage('Verification failed. The link may have expired.');
+        console.log(message);
+      }
+    };
+    verify();
+  }, [token]);
 
-//   return (
-//     <div>
-//       <h2>Email Verification</h2>
-//       <p>{message}</p>
-      // <p>hi this is email verification and ur mail will be verified now</p>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h2>Email Verification</h2>
+      <p>{message}</p>
+      <p>hi this is email verification and ur mail will be verified now</p>
+    </div>
+  );
+};
 
 
-// export default verifyEmail;
+export default verifyEmail;
